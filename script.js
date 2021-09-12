6b01650896d203eb39e8b5b7a844dadc75636fcc
AMIT KUMAR MISHRA <71893015+Amit0617@users.noreply.github.com>
2021-09-12 10:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ xhr.open("GET", url);
 xhr.setRequestHeader("Accept", "application/json");
 
 xhr.onload = function() {
-    var obj = JSON.parse(this.responseText);
+    var obj = JSON.parse(xhr.responseText);
     var address = obj.address;     
     //let text = "<table border='3'>"
     //for (let x in myObj) {
